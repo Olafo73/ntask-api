142ffbb59b1f958b9d2e0a3aac0afa23ea53dc4b
Nacho Olavarrieta <nacho@chirboa.com>
2017-09-06 12:21:50 +0200

    Comienzo del despliege de rutas para hacer todas las acciones necesarias con las tareas. Se añade body-parser a la lista de requisitos para poder acceder al cuerpo del mensaje enviado al servidor y poder, de esta forma, establecer algunos controles de seguridad en los middleware de la información enviada en este cuerpo.

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,37 @@
 module.exports = app => {
   const Tasks = app.db.models.Tasks;
-  app.get("/tasks", (req, res) => {
-      Tasks.findAll({}).then(tasks => {
-        res.json({tasks: tasks});
+
+  app.route("/tasks")
+    .all((req, res, next) => {
+      // Middleware for preexecution of routes
+      delete req.body.id;
+      next();
+    })
+    .get((req, res) => {
+      // "/tasks": List tasks
+      Tasks.findAll({})
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .post((req, res) => {
+      // "/tasks": Save new task
+    });
+
+    app.route("/tasks/:id")
+      .all((req, res, next) => {
+        // Middleware for preexecution of routes
+        delete req.body.id;
+        next();
+      })
+      .get((req, res) => {
+        // "/tasks/1": Find a task
+      })
+      .put((req, res) => {
+        // "/tasks/1": Update a task
+      })
+      .delete((req, res) => {
+        // "/tasks/1": Delete a tesk
       });
-  });
 };
