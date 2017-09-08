27b4b75fd6394f783ae7a83c663ba8358f305c8e
Nacho Olavarrieta <nacho@chirboa.com>
2017-09-08 11:08:06 +0200

    Arreglados los problemas para iniciar los test y actualizadas las versiones de algunos de los módulos de los que depende la app.

--- test/routes/index.js
+++ test/routes/index.js
@@ -4,8 +4,8 @@ describe("Routes: Index", () => {
       request.get("/")
         .expect(200)
         .end((err, res) => {
-          const expected = {status: "NTask API"};
-          expect(req.body).to.eql(expected);
+          const expected = {status: "NTask API "};
+          expect(res.body).to.eql(expected);
           done(err);
         });
     });
